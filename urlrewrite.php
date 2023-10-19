344fb2f6470710732a2be36d81941ff9b03c0b9e
fredbobjim <40921284+fredbobjim@users.noreply.github.com>
2023-10-19 13:24:29 +0300

    Add files via upload

---
+++ urlrewrite.php
@@ -0,0 +1,51 @@
+<?php
+$arUrlRewrite=array (
+  4 => 
+  array (
+    'CONDITION' => '#^/for-colleagues/vacancies/#',
+    'RULE' => '',
+    'ID' => 'bitrix:catalog',
+    'PATH' => '/for-colleagues/vacancies/index.php',
+    'SORT' => 100,
+  ),
+  5 => 
+  array (
+    'CONDITION' => '#^/news/media-about-us/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/news/media-about-us/index.php',
+    'SORT' => 100,
+  ),
+  3 => 
+  array (
+    'CONDITION' => '#^/education/#',
+    'RULE' => '',
+    'ID' => 'bitrix:catalog',
+    'PATH' => '/education/index.php',
+    'SORT' => 100,
+  ),
+  0 => 
+  array (
+    'CONDITION' => '#^/services/#',
+    'RULE' => '',
+    'ID' => 'bitrix:catalog',
+    'PATH' => '/services/index.php',
+    'SORT' => 100,
+  ),
+  1 => 
+  array (
+    'CONDITION' => '#^/catalog/#',
+    'RULE' => '',
+    'ID' => 'bitrix:catalog',
+    'PATH' => '/catalog/index.php',
+    'SORT' => 100,
+  ),
+  2 => 
+  array (
+    'CONDITION' => '#^/news/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/news/index.php',
+    'SORT' => 100,
+  ),
+);
